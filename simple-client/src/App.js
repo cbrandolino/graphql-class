cc18a8695da3465b2cc4b7e22c5d51bb9172d0e6
Claudio Brandolino <cbrandolino@gmail.com>
2019-06-26 16:21:53 +0100

    Add simple client

--- simple-client/src/App.js
+++ simple-client/src/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const useGreeting = () => {
+  const [ greeting, setGreeting ] = useState([]);
+  const fetchGreeting = async () => {
+    const response = await fetch('http://localhost.thenbs.io:9000/graphql', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({ query: '{ greeting }' })
+    });
+    const parsedResponse = await response.json();
+    setGreeting(parsedResponse);
+  };
+
+  useEffect(() => {
+    fetchGreeting();
+  }, []);
+  return greeting;
+};
+
+
 function App() {
+
+  const greeting = useGreeting();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          { JSON.stringify(greeting) }
         </p>
         <a
           className="App-link"
