6b8b6154e0c7138fca16ea4dbe3dbb736b254392
Claudio Brandolino <cbrandolino@gmail.com>
2019-06-30 15:49:14 +0100

    Add resolvers for creatures

--- simple-server/src/resolvers.js
+++ simple-server/src/resolvers.js
@@ -2,37 +2,45 @@ const { getPokedex } = require('./Pokedex');
 
 const urlToId = url => url.split('/')[6];
 
+const getPokemonIds = pokemon => pokemon.map(({ pokemon: { url }}) => urlToId(url));
+
 const creatureResolver = async (_, { id }, { dataSources }) => {
-  const { name, sprites, types } = await dataSources.pokemonAPI.getCreature(id);
+  const { name, sprites } = await dataSources.pokemonAPI.getCreature(id);
   return {
     name,
     sprite: sprites.front_default,
     id,
-    creatureTypes: types.map(({ type: { name, url }}) => ({ name, id: urlToId(url)}))
   }
 }
+
+const creatureTypeResolver = async (_, { id: typeID }, { dataSources }) =>
+  await dataSources.pokemonAPI.getCreatureType(typeID);
+
+const creatureTypesResolver = (creatureTypes, context) =>
+  Promise.all(creatureTypes.map(async id => creatureTypeResolver(null, { id }, context)));
+
+const creaturesResolver = (creatures, context) =>
+  Promise.all(creatures.slice(0, 5).map(async id => creatureResolver(null, { id }, context)));
 
 const resolvers = {
   Query: {
     creature: creatureResolver,
-    creatureType: async (_, { id: typeID }, { dataSources }) => {
-      const { name, id, pokemon } = await dataSources.pokemonAPI.getCreatureType(typeID);
-      return {
-        name,
-        id,
-        creatures: pokemon.map(({ pokemon: { name, url }}) => ({ name, id: urlToId(url)})),
-      }
-    },
+    creatureType: creatureTypeResolver,
     pokedex: async (_, { id: pokedexId }) => {
         const pokedex = await getPokedex(pokedexId);
         return pokedex;
     }
   },
   Pokedex: {
-      creatures: async ({ creatures }, _, context) => {
-          const results = await Promise.all(creatures.map(async id => creatureResolver(null, { id: 1 }, context)));
-          return results;
-      }
+    creatures: async ({ creatures }, _, context) => await creaturesResolver(creatures, context),
+  },
+  CreatureType: {
+    creatures: async ({ pokemon }, _, context) =>
+      await creaturesResolver(getPokemonIds(pokemon), context),
+  },
+  Creature: {
+    creatureTypes: async ({ creatureTypes }, _, context) =>
+      await creatureTypesResolver(creatureTypes.map(({ id }) => id), context)
   }
 }
 
