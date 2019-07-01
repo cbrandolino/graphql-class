98c5e6fcf4e13e239367727c8f18a0b658bcc5c5
Claudio Brandolino <cbrandolino@gmail.com>
2019-07-01 10:55:01 +0100

    Make current server work with apollo-server-micro

--- simple-server/index.js
+++ simple-server/index.js
@@ -1,34 +1,8 @@
 const cors = require('micro-cors')();
 const { ApolloServer, gql } = require('apollo-server-micro');
-
-const typeDefs = gql`
-  type Query {
-    sayHello: String
-  }
-`;
-
-const resolvers = {
-  Query: {
-    sayHello(parent, args, context) {
-      return 'Hello World!';
-    },
-  },
-};
-
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
-module.exports = cors(apolloServer.createHandler()); 
-
-/*
-const cors = require('cors');
-const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
 const PokemonAPI = require('./src/PokemonAPI');
 const typeDefs = require('./src/typeDefs');
 const resolvers = require('./src/resolvers');
-
-const port = 9000;
-
-const app = express();
 
 const apolloServer = new ApolloServer({
   typeDefs,
@@ -36,12 +10,8 @@ const apolloServer = new ApolloServer({
   dataSources: () => ({
     pokemonAPI: new PokemonAPI(),
   }),
-  playground: {
-    endpoint: '/graphql',
-  }
+  playground: true,
+  introspection: true,
 });
 
-apolloServer.applyMiddleware({ app });
-app.use(cors());
-app.listen(port, () => console.log(`server running on port ${port}`));
-*/
+module.exports = cors(apolloServer.createHandler()); 
